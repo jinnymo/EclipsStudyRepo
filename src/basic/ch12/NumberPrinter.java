80f6367260390aa12000f6b68f037016cfd58a5f
k_dy <ehddbs1105@gmail.com>
2024-04-18 14:02:49 +0900

    .

--- src/basic/ch12/NumberPrinter.java
+++ src/basic/ch12/NumberPrinter.java
@@ -2,4 +2,15 @@ package basic.ch12;
 
 public class NumberPrinter {
 
+	private int id;
+	private static int waitNumber;
+	
+	public NumberPrinter(int id ) {
+		this.id = id;
+		waitNumber =1;
+	}
+	public void printWaitNumber() {
+		System.out.println(id + "번에 기기의 대기 순번은 " + waitNumber);
+		waitNumber++;
+	}
 }
---
+++ src/basic/ch12/NumberPrinterTest.java
@@ -0,0 +1,22 @@
+package basic.ch12;
+
+public class NumberPrinterTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		NumberPrinter n1 = new NumberPrinter(1);//왼쪽 기기
+		NumberPrinter n2 = new NumberPrinter(2);//오른쪽기기
+		n1.printWaitNumber();//고객1 
+		n1.printWaitNumber();//고객2 
+		n1.printWaitNumber();//고객3 
+		n1.printWaitNumber();//고객4
+	
+		n2.printWaitNumber();//고객5
+		n2.printWaitNumber();//고객6
+		n2.printWaitNumber();//고객7
+		n2.printWaitNumber();//고객8
+	
+	}
+
+}
